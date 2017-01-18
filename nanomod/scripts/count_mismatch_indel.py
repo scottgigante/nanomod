e7b1fed379d2f5d112b9015f2b5438f018985a44
Scott Gigante <scottgigante@gmail.com>
2017-01-19 10:50:56 +1100

    added extra information to bam verification

--- nanomod/scripts/count_mismatch_indel.py
+++ nanomod/scripts/count_mismatch_indel.py
@@ -26,6 +26,7 @@
 
 import sys
 import csv
+csv.field_size_limit(sys.maxsize)
 
 def run(filename):
 	with open(file, 'r') as bamfile:
@@ -34,6 +35,8 @@ def run(filename):
 		total_insert = 0
 		total_delete = 0
 		total_read = total_genome = 0
+		longest_read = ""
+		longest_read_length = 0
 		reads = 0
 		fail_flags=[4]
 		cigar_codes = ['S','H','M','D','I']
@@ -45,6 +48,7 @@ def run(filename):
 				if flag not in fail_flags:
 					cigar = line[5]
 					n = ""
+					read_length = 0
 					for i in cigar:
 						if i not in cigar_codes:
 							n = n + i
@@ -54,10 +58,10 @@ def run(filename):
 							n = ""
 						elif i == 'I':
 							total_insert += int(n)
-							total_read += int(n)
+							read_length += int(n)
 							n = ""
 						elif i == 'M':
-							total_read += int(n)
+							read_length += int(n)
 							total_genome += int(n)
 							n = ""
 						else:
@@ -66,13 +70,21 @@ def run(filename):
 					score = int(line[13].split(':')[-1])
 					total_edit += edit_dist
 					total_score += score
+					total_read += read_length
+					print line[0] + "\t" + str(read_length)
+					if read_length > longest_read_length:
+						longest_read = line[0]
+						longest_read_length = read_length
 					reads += 1
 			except IndexError:
 				print line
 	return { 'averageEditDistance' : float(total_edit)/reads, 
 			 'averageAlignmentScore' : float(total_score)/reads,
 			 'insertionPercent' : float(total_insert)/total_read,
-			 'deletionPercent' : float(total_delete)/total_genome }
+			 'deletionPercent' : float(total_delete)/total_genome,
+			 'averageReadLength' : float(total_read)/reads,
+			 'longestRead' : longest_read,
+			 'longestReadLength' : longest_read_length }
 			
 
 if __name__ == "__main__":
