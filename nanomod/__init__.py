91b31ac0c8d16463836e1771b5fe5016b6ab79da
Scott Gigante <scottgigante@gmail.com>
2017-01-19 10:51:01 +1100

    updated license

--- nanomod/__init__.py
+++ nanomod/__init__.py
@@ -1,8 +1,19 @@
 ################################################################################
 #                                                                              #
-# __init__.py: initialise Python package.                                      #
+# Nanomod is free software: you can redistribute it and/or modify              #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
 #                                                                              #
-# This file is part of Nanomod. Say something about a GNU licence?             #
+# Nanomod is distributed in the hope that it will be useful,                   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with Nanomod.  If not, see <http://www.gnu.org/licenses/>.             #
+#                                                                              #
+# __init__.py: initialise Python package.                                      #
 #                                                                              #
 # Author: Scott Gigante                                                        #
 # Contact: gigante.s@wehi.edu.au                                               #
--- nanomod/__main__.py
+++ nanomod/__main__.py
@@ -1,11 +1,22 @@
 ################################################################################
 #                                                                              #
+# Nanomod is free software: you can redistribute it and/or modify              #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# Nanomod is distributed in the hope that it will be useful,                   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with Nanomod.  If not, see <http://www.gnu.org/licenses/>.             #
+#                                                                              #
 # __main__.py: Runs Scott Gigante's Nanomod.                                   #
 # Input: Reference genome and fast5 reads                                      #
 # Output: fasta, bam, eventalign, labelled fast5 reads and (eventually) a      #
 # trained network for nanonetcall                                              #
-#                                                                              #
-# This file is part of Nanomod. Say something about a GNU licence?             #
 #                                                                              #
 # Author: Scott Gigante                                                        #
 # Contact: gigante.s@wehi.edu.au                                               #
