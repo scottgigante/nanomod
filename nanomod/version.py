4881e99b14a8661bf851e131b843b568c858f3ad
Scott Gigante <scottgigante@gmail.com>
2017-02-22 09:24:35 +1100

    add binary installer

---
+++ nanomod/version.py
@@ -0,0 +1,24 @@
+################################################################################
+#                                                                              #
+# Nanomod is free software: you can redistribute it and/or modify              #
+# it under the terms of the GNU General Public License as published by         #
+# the Free Software Foundation, either version 3 of the License, or            #
+# (at your option) any later version.                                          #
+#                                                                              #
+# Nanomod is distributed in the hope that it will be useful,                   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of               #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                #
+# GNU General Public License for more details.                                 #
+#                                                                              #
+# You should have received a copy of the GNU General Public License            #
+# along with Nanomod.  If not, see <http://www.gnu.org/licenses/>.             #
+#                                                                              #
+# __init__.py: initialise Python package.                                      #
+#                                                                              #
+# Author: Scott Gigante                                                        #
+# Contact: gigante.s@wehi.edu.au                                               #
+# Date: 06 Jan 2017                                                            #
+#                                                                              #
+################################################################################
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+version_py = os.path.join(os.path.dirname(__file__), 'nanomod', '__init__.py')
+version = open(version_py).read().strip().split('=')[-1].replace('"','').strip()
+print version
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = 'nanomod',
+  packages = ['nanomod'],
+  package_dir={'nanomod': "nanomod"},
+  version=version,
+  install_requires=['h5py>2.2.0'],
+  requires=['python (>=2.7, <3.0)'],
+  description = 'A tool designed to call base modifications on Oxford Nanopore Technologies\' MinION sequencing data.',
+  long_description=read('README.md'),
+  author = 'Scott Gigante',
+  author_email = 'scottgigante@gmail.com',
+  url = 'https://github.com/scottgigante/nanomod', # use the URL to the github repo
+  keywords = ['nanopore', 'methylation', 'epigenomics', 'hdf5', 'fast5', 'oxford', 'minion', 'basecalling'], # arbitrary keywords
+  classifiers = [
+		"Development Status :: 3 - Alpha",
+		'Intended Audience :: Science/Research',
+		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'Topic :: Scientific/Engineering :: Bio-Informatics'
+		],
+  entry_points = {
+        'console_scripts': ['nanomod = nanomod.__main__:run'],
+    },
+)
