0f96fa04042b658c4da20462511b1e0e176c1b27
Scott Gigante <scottgigante@gmail.com>
2017-02-22 10:00:34 +1100

    add versioning

--- nanomod/__init__.py
+++ nanomod/__init__.py
@@ -24,8 +24,8 @@
 import subprocess
 import os
 import json
+from version import __version__
 
-__version__ = '0.1.0'
 __version_info__ = tuple([int(num) for num in __version__.split('.')])
 
 __exe__ = {}
