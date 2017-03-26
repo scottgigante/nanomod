3e3912c0a45cd99de775e24d3aba1106ff985d5b
Scott Gigante <scottgigante@gmail.com>
2017-03-26 15:46:19 +1100

    document utils

--- nanomod/utils.py
+++ nanomod/utils.py
@@ -33,55 +33,64 @@ import logging
 
 __log_levels__ = [logging.WARNING, logging.INFO, logging.DEBUG]
 
-# set up logging
 def configureLog(level=0):
+    """
+    Set up logging based on user input to verbosity
+    :param level: Positive integer, capped at 2
+    """
     if level >= len(__log_levels__):
+        # too high, not available
         level = len(__log_levels__) - 1
     elif level < 0:
+        # too low, not available
         level = 0
     logging.basicConfig(format='[%(levelname)s] %(message)s', level=__log_levels__[level])
 
-def preventOverwrite(file, options):
-    """ Check if we are accidentally overwriting something
-    @param file Path to file to be written
-    @param options Namespace from argparse """
+def preventOverwrite(file, force):
+    """
+    Check if we are accidentally overwriting something
+    :param file: Path to file to be written
+    :param force: Boolean value, force overwrite or not
+    :returns: Boolean value, prevent overwrite or not
+    """
     if file is not None and os.path.isfile(file):
-        if options.force:
+        if force:
             logging.info("{} already exists. Forcing recreation.".format(file))
         else:
             logging.warning("{} already exists. Use --force to recompute.".format(file))
             return True
     return False
 
-# call subprocess to create a new file using an external script if the file 
-# doesn't already exist
-#
-# TODO: can we move away from shell calls?
-#
-# @args call The system call to be run
-# @args options Namespace object from argparse
-# @args newFile File to be created
-# @args outputFile File to which we shall pipe stdout
-# @args close_fds Boolean to close outputFile after writing
-# @args shell Use local shell
-# @args mode Writing mode: choose from w, wb, a
-def callSubProcess(call, options, newFile=None, outputFile=None, close_fds=True, 
+def callSubProcess(call, force, newFile=None, outputFile=None, close_fds=True, 
         shell=True, mode='w'):
+    """
+    Call subprocess to create a new file using an external script if the file 
+    doesn't already exist
+    TODO: can we move away from shell calls?
+    
+    :param call: The system call to be run as a string or array_like
+    :param: Boolean value, True if we want to overwrite extant files, False otherwise
+    :param newFile: File that should be created after run
+    :param outputFile: File to which we shall pipe stdout
+    :param close_fds: Boolean value, close outputFile after writing or not
+    :param shell: Use local shell call - true for string call, false for array_like call
+    :param mode: Writing mode: choose from w, wb, a
+    """
     try:
         # we should use shell=False to make this more portable
         # print call to debug
         logging.info(call if shell else " ".join(call))
     
-        if preventOverwrite(newFile, options):
+        if preventOverwrite(newFile, force):
             return 1
         
         if outputFile is not None:
             out=open(outputFile, mode)
-        elif options.verbosity < 2:
+        elif logging.getLogger().isEnabledFor(logging.DEBUG):
+            out=sys.stdout # TODO: can we send this to logging?
+        else:
             # don't print stdout from subprocess
             out=open(os.devnull, 'w')
-        else:
-            out=sys.stdout
         subprocess.call(call, stdout=out, close_fds=close_fds, shell=shell)
     
         # check the file was created, if given
@@ -92,11 +101,11 @@ def callSubProcess(call, options, newFile=None, outputFile=None, close_fds=True,
         raise e
     return 0
 
-# create a directory, if it doesn't already exist
-#
-# @args dir The directory to be created
-# @return None    
 def makeDir(dir):
+    """
+    Create a directory, if it doesn't already exist
+    :param dir: The directory to be created
+    """
     try:
         os.mkdir(dir)
     except OSError:
@@ -104,19 +113,31 @@ def makeDir(dir):
         pass
 
 def loadJson(filename):
+    """
+    Load a JSON file
+    :param filename: string File to be loaded
+    :returns: dictionary or array_like JSON output
+    """
     with open(filename, 'r') as inFh:
         data = json.load(inFh)
     return data
 
 def saveJson(filename, data):
+    """
+    Save a JSON file
+    :param filename: string File to be loaded
+    :param data: dictionary or array_like JSON input
+    """
     with open(filename, 'w') as outFh:
         json.dump(data, fp=outFh, indent=4)
-        
-# multiprocessing.Pool.map() wrapper for functions taking more than one argument
-# @args func the function to be called
-# @args args an array of arguments for the function
-# @return return value of called function
+
 def multiprocessWrapper(func, args):
+    """
+    multiprocessing.Pool.map() wrapper for functions taking more than one argument
+    :param func: the function to be called
+    :param args: an array of arguments for the function
+    :returns: return value of called function
+    """
     try:
         return func(*args)
     except Exception as e:
