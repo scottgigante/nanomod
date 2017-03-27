2a70e9693af5d5eb351235da0ce2b8f06b023724
Scott Gigante <scottgigante@gmail.com>
2017-03-27 16:33:23 +1100

    bugfix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages()
+from setuptools import setup, find_packages
 
 version_py = os.path.join(os.path.dirname(__file__), 'nanomod', '__init__.py')
 version = open(version_py).read().strip().split('=')[-1].replace('"','').strip()
