3c742e8853890b9b90e4c7693a5fb65b0376bd38
Scott Gigante <scottgigante@gmail.com>
2017-03-28 13:46:23 +1100

    config travis with absolute paths

--- nanomod/parse_args.py
+++ nanomod/parse_args.py
@@ -251,7 +251,6 @@ def parseCommandArgs(command, argv):
     options = parser.parse_args(argv)
     configureLog(options.verbosity)
     logging.debug("nanomod {} {}".format(command, " ".join(argv)))
-    logging.debug([os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(os.getcwd())) for f in fn])
 
     initialiseArgs(command, options)
 
