d3f0bc8271e9a52e76ebeca2ff08ce003982cf22
Scott Gigante <scottgigante@gmail.com>
2017-03-29 14:14:24 +1100

    doc setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ version_py = os.path.join(os.path.dirname(__file__), 'nanomod', 'version.py')
 version = open(version_py).read().strip().split('=')[-1].replace('"','').strip()
 print version
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
+    """
+    Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ...
+
+    :param fname: string Path to file
+
+    :returns: string File contents
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
