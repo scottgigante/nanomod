35f825aa6183da508beadfa8853ef70a556d7175
Scott Gigante <scottgigante@gmail.com>
2017-04-03 16:11:20 +1000

    remove temp file creation

--- nanomod/embed_eventalign.py
+++ nanomod/embed_eventalign.py
@@ -154,103 +154,8 @@ def writeFast5(options, events, fast5Path, initial_ref_index, last_ref_index,
 
     return passQuality
 
-def processRead(options, idx, fast5Path, genome, modified, kmer, alphabet):
-    """
-    Step through eventalign data corresponding to a fast5 file and label events
-
-    :param options: Namespace object from argparse
-    :param idx: headers index of eventalign file
-    :param fast5Path: path to original fast5 file
-    :param genome: Dictionary containing genome records
-    :param modified: Boolean value, whether or not this fast5 has modified motif
-    :param kmer: int length of kmer labels
-    :param alphabet: array of base labels in alphabet
-
-    :returns: two-element array containing basename of fast5 file and boolean indicating read quality
-    """
-
-    fast5File = fast5Path.split('/')[-1]
-    try:
-        eventalign = np.load(os.path.join(options.tempDir, fast5File + ".npy"))
-    except IOError:
-        logging.warning("Failed to load {}.npy".format(os.path.join(options.tempDir, fast5File)))
-        # why on earth?! we just created this file!
-        return [fast5File, False]
-    skip = False
-
-    # open fast5 file
-    try:
-        fast5 = h5py.File(fast5Path, 'r')
-        # r9 for now
-        events = fast5.get("Analyses/Basecall_1D_000/BaseCalled_template/Events").value
-        events = append_fields(events, 'kmer', ['X'*kmer] * events.shape[0])
-        events = append_fields(events, 'seq_pos', [-1] * events.shape[0]) # is there a better option here? keep counting?
-    except IOError:
-        logging.warning("Failed to read {}".format(fast5Path))
-        return [fast5File, False]
-    except TypeError:
-        logging.warning("Data corrupted in {}".format(fast5Path))
-        return [fast5File, False]
-
-    try:
-        readLength = fast5.get("Analyses/Basecall_1D_000/Summary/basecall_1d_template").attrs["sequence_length"]
-    except AttributeError:
-        # attributes not found
-        readLength = options.readLength
-    initial_event_index = int(eventalign[0][idx['event_index']])
-    initial_ref_index = int(eventalign[0][idx['ref_pos']])
-    last_ref_index = initial_ref_index-1
-    chromosome = eventalign[0][idx['contig']]
-    start=False
-    numSkips = 0
-    numStays = 0
-
-    # check strand direction
-    i = 0
-    while int(eventalign[0][idx['event_index']]) == int(eventalign[1][idx['event_index']]):
-        i += 1
-    forward = int(eventalign[i][idx['event_index']]) < int(eventalign[i + 1][idx['event_index']])
-
-    for line in eventalign:
-
-        current_ref_index = int(line[idx['ref_pos']])
-
-        seq_pos_diff = abs(current_ref_index - last_ref_index)
-        try:
-            seq_pos = last_seq_pos + seq_pos_diff
-        except NameError:
-            # don't have a last_seq_pos, first event
-            seq_pos = 0
-
-        seq = getKmer(genome, chromosome, current_ref_index, kmer, forward)
-        if options.rate > 0:
-        	seq = randomPermuteSeq(seq, alphabet, options.rate)
-
-        if last_ref_index == current_ref_index:
-            numStays += 1
-        elif abs(last_ref_index - current_ref_index) > 1:
-            # count the size of the skip
-            # skipping two bases at once counts as two skips
-            numSkips += abs(last_ref_index - current_ref_index) - 1
-        last_ref_index = current_ref_index
-        last_seq_pos = seq_pos
-
-        events["seq_pos"][int(line[idx['event_index']])] = seq_pos
-        events["kmer"][int(line[idx['event_index']])] = seq
-
-    fast5.close()
-    try:
-        pass_quality = writeFast5(options, events, fast5Path, initial_ref_index,
-                last_ref_index, chromosome, forward, numSkips, numStays,
-                readLength, kmer, genome)
-    except Exception as e:
-        logging.warning("Failed to save {}.".format(fast5File))
-        logging.warning(str(e))
-        return [fast5File, False]
-    return [fast5File, pass_quality]
-
-def processEventalignWorker(options, eventalign, refs, idx, numReads, start, stop):
-    """
+def processEventalignWorker(options, eventalign, refs, idx, numReads, start, stop, genome, modified, kmer, alphabet):
+    """fast5Path, genome, modified, kmer, alphabet
     Write temporary .npy files to split eventalign tsv file into chunks
     corresponding to one fast5 file each - this allows multiprocessing later on.
 
@@ -260,8 +165,12 @@ def processEventalignWorker(options, eventalign, refs, idx, numReads, start, sto
     :param idx: dictionary linking headers with positions in the tsv
     :param start: int Starting line at which to begin processing (0-based)
     :param end: int Ending line at which to end processing (1-based)
+    :param genome: Dictionary containing genome records
+    :param modified: Boolean value, whether or not this fast5 has modified motif
+    :param kmer: int length of kmer labels
+    :param alphabet: array of base labels in alphabet
 
-    :returns filenames: array_like list of paths to fast5 files which have been processed
+    :returns output: array of two-element arrays containing basename of fast5 file and boolean indicating read quality
     :returns premadeFilenames: list of basenames of fast5 files which had already been processed prior to this call to Nanomod
     """
 
@@ -271,8 +180,9 @@ def processEventalignWorker(options, eventalign, refs, idx, numReads, start, sto
         current_read_name = ""
         skip=True
         started=False if start > 0 else True
-        tmp=None
+        events=None
         filenames = set()
+        output = list()
         premadeFilenames = set()
         n=0
 
@@ -288,21 +198,31 @@ def processEventalignWorker(options, eventalign, refs, idx, numReads, start, sto
                     started=True
                     skip=True
                     continue
-                if tmp is not None and len(tmp) > 0:
+
+                #########################
+                # save previous read
+                #########################
+                if events is not None:
                     try:
-                        np.save(filename,tmp)
-                        logging.debug("Saving {}.npy".format(filename))
+                        passQuality = writeFast5(options, events, fast5Path,
+                            initial_ref_index, last_ref_index, chromosome, forward,
+                            numSkips, numStays, readLength, kmer, genome)
+                        output.append([fast5Name, passQuality])
+                        filenames.add(fast5Name)
+                        events = None
                         n += 1
                         if numReads > 0 and n >= numReads:
                             break
-                    except IOError as e:
-                        logging.warning("Failed to write {}.npy: {}".format(filename, e))
+                    except Exception as e:
+                        logging.warning("Failed to save {}.".format(fast5File))
+                        logging.warning(str(e))
                 if stop is not None and i > stop:
                     # we're done!
                     break
 
+                ############################
                 # new read, initialise
-                tmp = []
+                ############################
                 current_read_name = line[idx['read_name']]
                 try:
                     fast5Path = refs[current_read_name]
@@ -316,6 +236,9 @@ def processEventalignWorker(options, eventalign, refs, idx, numReads, start, sto
                     # not template, skip
                     skip=True
                     continue
+                elif fast5Name in filenames or fast5Name in premadeFilenames:
+                    skip = True
+                    continue
 
                 outfile = getOutfile(fast5Name, options)
                 filename = os.path.join(options.tempDir, fast5Name)
@@ -323,25 +246,95 @@ def processEventalignWorker(options, eventalign, refs, idx, numReads, start, sto
                     skip=True
                     premadeFilenames.add(fast5Name)
                     continue
-                elif preventOverwrite(filename + ".npy", options.force, logging.debug):
+
+                # open fast5 file
+                try:
+                    fast5 = h5py.File(fast5Path, 'r')
+                    # r9 for now
+                    events = fast5.get("Analyses/Basecall_1D_000/BaseCalled_template/Events").value
+                    events = append_fields(events, 'kmer', ['X'*kmer] * events.shape[0])
+                    events = append_fields(events, 'seq_pos', [-1] * events.shape[0]) # is there a better option here? keep counting?
+                except IOError:
+                    logging.warning("Failed to read {}".format(fast5Path))
                     skip=True
-                    filenames.add(fast5Path)
                     continue
+                except TypeError:
+                    logging.warning("Data corrupted in {}".format(fast5Path))
+                    skip=True
+                    continue
+
+                try:
+                    readLength = fast5.get("Analyses/Basecall_1D_000/Summary/basecall_1d_template").attrs["sequence_length"]
+                except AttributeError:
+                    # attributes not found
+                    readLength = options.readLength
+                initial_event_index = int(line[idx['event_index']])
+                initial_ref_index = int(line[idx['ref_pos']])
+                last_ref_index = initial_ref_index-1
+                chromosome = line[idx['contig']]
+                numSkips = 0
+                numStays = 0
+                forward = None
+                seq_pos = None
+                last_seq_pos = None
 
                 filenames.add(fast5Path)
                 skip=False
 
-            tmp.append(line)
+            ########################
+            # process line
+            ########################
+
+            # check strand direction
+            if forward is None:
+                if int(line[idx['event_index']]) == initial_ref_index:
+                    continue
+                else:
+                    forward = initial_ref_index < int(line[idx['event_index']])
+            current_ref_index = int(line[idx['ref_pos']])
+
+            # generate seq pos
+            seq_pos_diff = abs(current_ref_index - last_ref_index)
+            try:
+                seq_pos = last_seq_pos + seq_pos_diff
+            except TypeError:
+                # don't have a last_seq_pos, first event
+                seq_pos = 0
+
+            # pull seq from genome
+            seq = getKmer(genome, chromosome, current_ref_index, kmer, forward)
+            if options.rate > 0:
+                seq = randomPermuteSeq(seq, alphabet, options.rate)
+
+            # count skips and stays
+            if last_ref_index == current_ref_index:
+                numStays += 1
+            elif abs(last_ref_index - current_ref_index) > 1:
+                # count the size of the skip
+                # skipping two bases at once counts as two skips
+                numSkips += abs(last_ref_index - current_ref_index) - 1
+            last_ref_index = current_ref_index
+            last_seq_pos = seq_pos
+
+            # add data to events table
+            events["seq_pos"][int(line[idx['event_index']])] = seq_pos
+            events["kmer"][int(line[idx['event_index']])] = seq
 
         # last one gets missed
-        # TODO: should we exclude fail reads earlier to save time?
-        if tmp is not None and len(tmp) > 0:
-            np.save(filename,tmp)
-            logging.debug("Saving {}.npy".format(filename))
+        # TODO: can we exclude fail reads earlier to save time?
+        if events is not None:
+            try:
+                passQuality = writeFast5(options, events, fast5Path,
+                    initial_ref_index, last_ref_index, chromosome, forward,
+                    numSkips, numStays, readLength, kmer, genome)
+                output.append([fast5Name, passQuality])
+            except Exception as e:
+                logging.warning("Failed to save {}.".format(fast5File))
+                logging.warning(str(e))
 
-    return filenames, premadeFilenames
+    return output, premadeFilenames
 
-def processEventalign(options, eventalign, refs):
+def processEventalign(options, eventalign, refs, genome, modified, alphabet):
     """
     Write temporary .npy files to split eventalign tsv file into chunks
     corresponding to one fast5 file each - this allows multiprocessing later on.
@@ -350,10 +343,9 @@ def processEventalign(options, eventalign, refs):
     :param eventalign: String filename of eventalign tsv
     :param refs: dictionary linking read names and fast5 file paths
 
-    :returns filenames: array_like list of paths to fast5 files which have been processed
+    :returns output: array of two-element arrays containing basename of fast5 file and boolean indicating read quality
     :returns idx: dictionary linking headers with positions in the tsv
     :returns premadeFilenames: list of basenames of fast5 files which had already been processed prior to this call to Nanomod
-    :returns kmer: int length of kmer labels
     """
     if not os.path.exists(options.tempDir):
         os.makedirs(options.tempDir)
@@ -390,13 +382,13 @@ def processEventalign(options, eventalign, refs):
         splitRange = [nlines] + splitRange
 
         data = pool.map(processEventalignWorkerWrapper,
-                [[options, eventalign, refs, idx, -1, splitRange[i+1], splitRange[i]] for i in range(len(splitRange)-1)])
+                [[options, eventalign, refs, idx, -1, splitRange[i+1], splitRange[i], genome, modified, kmer, alphabet] for i in range(len(splitRange)-1)])
         pool.close()
         pool.join()
-        filenames, premadeFilenames = tuple(itertools.chain(*i) for i in itertools.izip(*data))
+        output, premadeFilenames = tuple(itertools.chain(*i) for i in itertools.izip(*data))
     else:
-        filenames, premadeFilenames = processEventalignWorker(options, eventalign, refs, idx, options.numReads, 0, None)
-    return filenames, idx, premadeFilenames, kmer
+        output, premadeFilenames = processEventalignWorker(options, eventalign, refs, idx, options.numReads, 0, None, genome, modified, kmer, alphabet)
+    return output, idx, premadeFilenames
 
 def checkPremade(options, filename):
     """
